5c772d05bc0549edb81fa3441069e5ae1c75db37
gcardoso <ger9047@gmail.com>
2019-11-05 02:07:54 -0600

    CRUD Completo en categorias

--- ModeloJson.php
+++ ModeloJson.php
@@ -157,11 +157,11 @@ class Datos extends Conexion
 			$user = array();
 			//le vamos pasando los atributos en codigficacion UTF8 por si tenemops algun caracter que no se encuentre en ingles como la ñ
 			//cada atributo es una posicion en el array user
-			$user["id"] = utf8_decode($id);
-			$user["usuario"] = utf8_decode($usuario);
-			$user["password"] = utf8_decode($password);
-			$user["role"] = utf8_decode($role);
-			$user["mail"] = utf8_decode($mail);
+			$user["id"] = utf8_encode($id);
+			$user["usuario"] = utf8_encode($usuario);
+			$user["password"] = utf8_encode($password);
+			$user["role"] = utf8_encode($role);
+			$user["mail"] = utf8_encode($mail);
 
 			//Para enviar las informacion 
 			//guardamos el array users 
@@ -190,6 +190,134 @@ class Datos extends Conexion
 
 
 		}
+
+
+
+		//CRUD para las categorias
+
+
+		public function createCategoriaModel($tabla){
+
+			$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla (titulo) VALUES (:titulo)");
+			//variables de apoyo
+			$titulo = "PERFUMES";
+
+		
+			$stmt->bindParam(":titulo", $titulo,PDO::PARAM_STR);
+
+		
+			if($stmt->execute()){
+				echo "registro exitoso";
+			}else{
+				echo "no se puede hacer el registro";
+			}
+
+
+
+		}
+
+
+		public function readCategoriasModel($tabla){
+
+			$stmt = Conexion::conectar()->prepare("SELECT id, titulo FROM $tabla");
+		
+		//vamos a llamar al metodo execute para ver si la sentencia se ejecuto correctamente 
+		$stmt->execute();
+
+		//llamamos al metodo bindColumn para llamar a los atributos de la tabla y se hace para cada uno de los campos
+		//para leer tenemos que guardar en variables digamos auxiliares
+		$stmt->bindColumn("id",$id);
+		$stmt->bindColumn("titulo",$titulo);
+	
+		//es una array para guardar los atributos en cada una de las posiciones
+		$categorias = array();
+		//bound es un patrametro de PDO
+
+			//probamos el funcionamiento 
+			//creamos una talba 
+
+			//Primer echo 
+			echo '
+			<table>
+			<tr>
+			<td><strong>id</strong></td>
+			<td><strong>titulo</strong></td>
+			 ';
+
+
+			 //Nuestro while es el encargado de mostrar los campos de los usuarios
+
+		while($fila = $stmt->fetch(PDO::FETCH_BOUND)){
+
+			$cat = array();
+			//le vamos pasando los atributos en codigficacion UTF8 por si tenemops algun caracter que no se encuentre en ingles como la ñ
+			//cada atributo es una posicion en el array user
+			$cat["id"] = utf8_encode($id);
+			$cat["titulo"] = utf8_encode($titulo);
+
+
+			//Para enviar las informacion 
+			//guardamos el array users 
+			//y acceder a ellos con estos arrays
+
+			array_push($categorias, $cat);
+
+			//Segundo echo 
+			echo '
+			<tr>
+			<td>'.$cat['id'].'</td>	
+			<td>'.$cat['titulo'].'</td>	
+			';
+
+		}
+
+
+			echo '</table>';
+
+					//retornamos nuestro array principal usuarios
+			return $categorias;
+
+
+
+
+		}
+
+
+		public function updateCategoriaModel($tabla){
+
+			$stmt = Conexion::conectar()->prepare("UPDATE $tabla  SET titulo = :titulo WHERE id = :id");
+			//variables de apoyo
+			$id = 17;
+			$titulo = "FUNDAS DE TELEFONO";
+
+			$stmt->bindParam(":id", $id, PDO::PARAM_INT);
+			$stmt->bindParam(":titulo", $titulo, PDO::PARAM_STR);
+
+		
+			if($stmt->execute()){
+				echo "actualización exitosa";
+			}else{
+				echo "no se puede hacer la actualización registro";
+			}
+
+		}
+
+
+		public function deleteCategoriaModel($id, $tabla){
+
+			$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id = :id");
+			$stmt->bindParam("id", $id, PDO::PARAM_INT);
+
+
+			if($stmt->execute()){
+				echo"Categoria eliminada exitosamente";
+			}else{
+				echo "La categoria no pudo ser eliminada";
+
+		
+			}
+		
+		}
 	
 	}
 
@@ -202,5 +330,5 @@ $obj = new Datos();
 //pasamos el parametro de la tabla usuario para la funcion crear usuario que gracias al prepare nos deja insertar el codigo sql 
 //cuanto estanciones nuestro objeto y le pasamos cada funcion para comprobar su funcionamiento 
 
-$obj->loginUsuariosModel("usuarios");
+$obj->deleteCategoriaModel(17, "categorias");
 ?>
